3495590c03f67f02e6c1100c3e76b4ea89be9246
Dr Maeve Murphy Quinlan <mmurphyquinlan@gmail.com>
2026-06-04 18:00:30 +0200

    Update filepaths in Aire data run

--- aire_runs/wales_submit_job_array.sh
+++ aire_runs/wales_submit_job_array.sh
@@ -15,8 +15,8 @@ python wales_run.py \
     "$SLURM_ARRAY_TASK_ID" \
     "wales_filenames.csv" \
     "parks_gardens_id/" \
-    "wales_aire_run_output/" \
-    "/Volumes/Extreme SSD/wales_lidar/wales_dtm_32bit_cog.tif" \
-    "/Volumes/Extreme SSD/wales_lidar/wales_dsm_32bit_cog.tif" \
+    "/mnt/scratch/earmmu/wales_aire_run_output/" \
+    "/mnt/scratch/earmmu/wales_lidar/wales_dtm_32bit_cog.tif" \
+    "/mnt/scratch/earmmu/wales_lidar/wales_dsm_32bit_cog.tif" \
     "../example_datasets/all_parks_ids.csv" \
     "../example_datasets/LUT_regions_authorities_filenames.geojson"
