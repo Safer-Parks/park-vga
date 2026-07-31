038b4bde2f20f84987148a21871ee0f2b3470360
Maeve Murphy Quinlan <mmurphyquinlan@gmail.com>
2026-07-31 12:56:29 +0200

    Update testing_height_england.sh

--- aire_runs/testing_height_england.sh
+++ aire_runs/testing_height_england.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=test_height_parks
-#SBATCH --array=3
+#SBATCH --array=2
 #SBATCH --time=20:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
-#SBATCH --output=logs/test_height_parks_%a.log
-#SBATCH --error=logs/test_height_parks_%a.err
+#SBATCH --output=logs/test_height_parks1_%a.log
+#SBATCH --error=logs/test_height_parks1_%a.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --account=proj_a
 
@@ -15,7 +15,7 @@ python england_run.py \
     "$SLURM_ARRAY_TASK_ID" \
     "testing_leeds_bradford.csv" \
     "parks_gardens_id/" \
-    "/mnt/scratch/earmmu/testing_height_aire_run_output/" \
+    "/mnt/scratch/earmmu/testing_height_aire_run_output_1/" \
     "/mnt/scratch/earmmu/england_lidar" \
     "/mnt/scratch/earmmu/england_lidar/DSM" \
     "../example_datasets/all_parks_ids.csv" \
