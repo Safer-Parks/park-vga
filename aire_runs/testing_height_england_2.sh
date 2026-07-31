e98c9eaa6f1a7937e5d788d105bf3bf7b93a11b7
Maeve Murphy Quinlan <mmurphyquinlan@gmail.com>
2026-07-31 12:57:23 +0200

    Update testing_height_england_2.sh

--- aire_runs/testing_height_england_2.sh
+++ aire_runs/testing_height_england_2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=test_height_parks
-#SBATCH --array=3
+#SBATCH --array=2
 #SBATCH --time=20:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
-#SBATCH --output=logs/test_height_parks_%a.log
-#SBATCH --error=logs/test_height_parks_%a.err
+#SBATCH --output=logs/test_height_parks2_%a.log
+#SBATCH --error=logs/test_height_parks2_%a.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --account=proj_a
 
